e32a141c435e717aa5e2b6fdd72450dfe3a7ac43
WSR-wsr <76859933+WSR-wsr@users.noreply.github.com>
2021-09-27 09:35:13 +0800

    Add files via upload

--- Dataset+Code/CV.py
+++ Dataset+Code/CV.py
@@ -8,45 +8,74 @@
 # CV1 represents rows, CV2 represents columns
 
 import numpy as np
-import random
+from sklearn.model_selection import KFold
+
 
 def kfold(data, k, row=0, col=0, cv=3):
     dlen = len(data)
-    if cv == 1:
-        lens = row
-    elif cv == 2:
-        lens = col
+    if cv != 4:
+        if cv == 1:
+            lens = row
+        elif cv == 2:
+            lens = col
+        else:
+            lens = dlen
+        test_res = []
+        train_res = []
+        d = list(range(lens))
+        kf = KFold(5, shuffle=True)
+        d = kf.split(d)
+        for i in d:
+            test_res.append(list(i[1]))
+            train_res.append(list(i[0]))
+        if cv == 3:
+            return train_res, test_res
+        else:
+            train_s = []
+            test_s = []
+            for i in range(k):
+                train_ = []
+                test_ = []
+                for j in range(dlen):
+                    if data[j][cv - 1] in test_res[i]:
+                        test_.append(j)
+                    else:
+                        train_.append(j)
+                train_s.append(train_)
+                test_s.append(test_)
+            return train_s, test_s
     else:
-        lens = dlen
-    d = list(range(lens))
-    random.shuffle(d)
-    test_n = lens // k
-    n = lens % k
-    test_res = []
-    train_res = []
-    for i in range(n):
-        test = d[i * (test_n + 1):(i + 1) * (test_n + 1)]
-        train = list(set(d) - set(test))
-        test_res.append(test)
-        train_res.append(train)
-    for i in range(n, k):
-        test = d[i * test_n + n:(i + 1) * test_n + n]
-        train = list(set(d) - set(test))
-        test_res.append(test)
-        train_res.append(train)
-    if cv == 3:
-        return train_res, test_res
-    else:
+        r = list(range(row))
+        c = list(range(col))
+        kf = KFold(5, shuffle=True)
+        r = kf.split(r)
+        c = kf.split(c)
+        r_test_res = []
+        r_train_res = []
+        c_test_res = []
+        c_train_res = []
+        for i in r:
+            r_test_res.append(list(i[1]))
+            r_train_res.append(list(i[0]))
+        for i in c:
+            c_test_res.append(list(i[1]))
+            c_train_res.append(list(i[0]))
         train_s = []
         test_s = []
         for i in range(k):
             train_ = []
             test_ = []
-            for j in range(dlen):
-                if data[j][cv - 1] in test_res[i]:
-                    test_.append(j)
-                else:
-                    train_.append(j)
+            for m in range(dlen):
+                flag_1 = False
+                flag_2 = False
+                if data[m][0] in r_test_res[i]:
+                    flag_1 = True
+                if data[m][1] in c_test_res[i]:
+                    flag_2 = True
+                if flag_1 and flag_2:
+                    test_.append(m)
+                if (not flag_1) and (not flag_2):
+                    train_.append(m)
             train_s.append(train_)
             test_s.append(test_)
         return train_s, test_s
--- Dataset+Code/DLDN.py
+++ Dataset+Code/DLDN.py
@@ -18,34 +18,39 @@ import pandas as pd
 from CV import get_one_hot, kfold
 from sklearn.metrics import auc, roc_curve, precision_recall_curve, accuracy_score, recall_score, f1_score, \
     precision_score
+import time as TIM
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
-time = 1
+os.environ["CUDA_VISIBLE_DEVICES"] = "5"
+time = 2
 k = 5
-max_batches = 2500
+
+s = 25
 N_FEATURES = 200
 FEATURE_SHAPE = (200,)
 dataset_label = "RNA_PRO"
 data_batch_size = 32
 mask_batch_size = 32
-s = 150
 s_p = 2
 phase_2_start = 200
 early_stopping_patience = 200
 
 # main program
 for mm in range(1, 6):
-    for cv in range(1, 4):
+    for cv in range(1, 5):
         data_file = './data' + str(mm) + '/data.csv'
         label_file = './data' + str(mm) + '/label.csv'
         data = pd.read_csv(data_file, header=None, index_col=None).to_numpy()
         label = pd.read_csv(label_file, index_col=None, header=None).to_numpy()
+        label_copy = label.copy()
         row, col = label.shape
-        a = np.array([(i, j) for i in range(row) for j in range(col) if label[i][j]])
-        b = np.array([(i, j) for i in range(row) for j in range(col) if label[i][j] == 0])
-        np.random.shuffle(b)
-        sample = len(a)
-        b = b[:sample]
+        if cv == 4:
+            c = np.array([(i, j) for i in range(row) for j in range(col)])
+        else:
+            a = np.array([(i, j) for i in range(row) for j in range(col) if label[i][j]])
+            b = np.array([(i, j) for i in range(row) for j in range(col) if label[i][j] == 0])
+            np.random.shuffle(b)
+            sample = len(a)
+            b = b[:sample]
         mPREs = np.array([])
         mACCs = np.array([])
         mRECs = np.array([])
@@ -54,14 +59,44 @@ for mm in range(1, 6):
         mF1 = np.array([])
 
         for j in range(time):
-            if cv == 3:
+            if cv == 4:
+                c_tr, c_te = np.array(kfold(c, k=k, row=row, col=col, cv=cv))
+            elif cv == 3:
                 a_tr, a_te = np.array(kfold(a, k=k, row=row, col=col, cv=cv))
                 b_tr, b_te = np.array(kfold(b, k=k, row=row, col=col, cv=cv))
             else:
                 c = np.vstack([a, b])
                 c_tr, c_te = np.array(kfold(c, k=k, row=row, col=col, cv=cv))
             for i in range(k):
-                if cv == 3:
+                if cv == 4:
+                    b_tr = []
+                    a_tr = []
+                    # print(c_tr[i])
+                    for ep in c_tr[i]:
+                        if label_copy[c[ep][0]][c[ep][1]]:
+                            b_tr.append(c[ep])
+                        else:
+                            a_tr.append(c[ep])
+                    b_te = []
+                    a_te = []
+                    for ep in c_te[i]:
+                        if label_copy[c[ep][0]][c[ep][1]]:
+                            b_te.append(c[ep])
+                        else:
+                            a_te.append(c[ep])
+                    b_te = np.array(b_te)
+                    b_tr = np.array(b_tr)
+                    a_te = np.array(a_te)
+                    a_tr = np.array(a_tr)
+                    np.random.shuffle(b_te)
+                    np.random.shuffle(a_te)
+                    np.random.shuffle(b_tr)
+                    np.random.shuffle(a_tr)
+                    a_tr = a_tr[:len(b_tr)]
+                    a_te = a_te[:len(b_te)]
+                    train_sample = np.vstack([a_tr, b_tr])
+                    test_sample = np.vstack([a_te, b_te])
+                elif cv == 3:
                     train_sample = np.vstack([np.array(a[a_tr[i]]), np.array(b[b_tr[i]])])
                     test_sample = np.vstack([np.array(a[a_te[i]]), np.array(b[b_te[i]])])
                 else:
@@ -77,9 +112,12 @@ for mm in range(1, 6):
                 X_te = data[test_land][:, :-1]
                 y_te = data[test_land][:, -1]
 
+                # max_batches = int(len(y_tr) / 2)
+                max_batches = 2500
                 y_tr = get_one_hot(y_tr.astype(np.int8), 2)
                 y_te = get_one_hot(y_te.astype(np.int8), 2)
-                fir = FeatureImportanceRank(FEATURE_SHAPE, s, data_batch_size, mask_batch_size, str_id=dataset_label)
+                fir = FeatureImportanceRank(FEATURE_SHAPE, s, data_batch_size, mask_batch_size,
+                                            str_id=dataset_label)
                 fir.create_dense_MLP([60, 30, 20, 2], "softmax", metrics=[keras.metrics.CategoricalAccuracy()],
                                      error_func=K.binary_crossentropy)
                 fir.MLP.set_early_stopping_params(phase_2_start, patience_batches=early_stopping_patience,
@@ -110,17 +148,25 @@ for mm in range(1, 6):
                 mAUPRs = np.append(mAUPRs, apr)
                 mF1 = np.append(mF1, f1)
 
-                print('In time {}, k = {}:'.format(j + 1, i + 1))
-                print('Precision is :{}'.format(Pre))
-                print('Recall is :{}'.format(rec))
-                print("ACC is: {}".format(acc))
-                print("F1 is: {}".format(f1))
+                curve_1 = np.vstack([fpr, tpr])
+                curve_1 = pd.DataFrame(curve_1.T)
+                curve_1.to_csv('./co_s/d' + str(mm) + 'c' + str(cv) + '_c' + str(au) + '.csv', header=None, index=None)
+
+                curve_2 = np.vstack([rec_, pre])
+                curve_2 = pd.DataFrame(curve_2.T)
+                curve_2.to_csv('./co_s/d' + str(mm) + 'c' + str(cv) + '_r' + str(apr) + '.csv', header=None, index=None)
+
+                # print('In time {}, k = {}:'.format(j + 1, i + 1))
+                # print('Precision is :{}'.format(Pre))
+                # print('Recall is :{}'.format(rec))
+                # print("ACC is: {}".format(acc))
+                # print("F1 is: {}".format(f1))
                 print("AUC is: {}".format(au))
                 print('AUPR is :{}'.format(apr))
 
-        toa = np.vstack([mPREs, mACCs, mRECs, mAUCs, mAUPRs, mF1])
+        toa = np.vstack([mAUCs, mAUPRs])
         toa = pd.DataFrame(toa)
-        toa.to_csv('res_data/cv' + str(cv) + '_data' + str(mm) + '.csv', header=None, index=None)
+        toa.to_csv('res_data/' + str(s) + '/cv' + str(cv) + '_data' + str(mm) + '.csv', header=None, index=None)
         PRE = mPREs.mean()
         ACC = mACCs.mean()
         REC = mRECs.mean()
@@ -143,8 +189,8 @@ for mm in range(1, 6):
         print('AUC is :{}±{}'.format(round(AUC, 4), round(AUC_err, 4)))
         print('AUPR is :{}±{}'.format(round(AUPR, 4), round(AUPR_err, 4)))
 
-        f_1 = open('res_cv.txt', 'a')
-        f_1.write('res_data/data' + str(mm) + 'cv' + str(cv) + ':\n')
+        f_1 = open('./co_s/' + str(s) + 'res_cv.txt', 'a')
+        f_1.write('data' + str(mm) + 'cv' + str(cv) + ':\n')
         f_1.write(str(round(PRE, 4)) + '±' + str(round(PRE_err, 4)) + '\t')
         f_1.write(str(round(REC, 4)) + '±' + str(round(REC_err, 4)) + '\t')
         f_1.write(str(round(ACC, 4)) + '±' + str(round(ACC_err, 4)) + '\t')
